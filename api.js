85a0f26b95c948d1f70bbb83876d9d9fc0f51c11
Agnei Silva <agnei.silva@confitec.com.br>
2020-05-01 22:05:37 -0300

    implementado busca de projetos usando js e bootstrap

---
+++ api.js
@@ -0,0 +1,12 @@
+function getReposGithub(user, successFuncCallback, errosFuncCallback){
+
+    $.ajax({
+        url: "https://api.github.com/users/"+user+"/repos", 
+        dataType: "json", 
+        headers: {"Accept": "application/vnd.github.inertia-preview+json"},
+        error: errosFuncCallback,
+        success: successFuncCallback
+    });  
+}
+
+
